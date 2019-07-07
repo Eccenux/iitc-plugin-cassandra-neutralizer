c327fdd9aa64f02fa8f94f90e08bc2b4ad640060
Eccenux <eccenux+git@gmail.com>
2019-07-07 14:20:32 +0200

    Collecting portal stats for large areas

--- area and portals count in polygon.js
+++ area and portals count in polygon.js
@@ -1,5 +1,6 @@
-//
+// -----------------------------
 // Calculate visible polygon area
+// -----------------------------
 var polygons = window.plugin.cassandraNeutralizer.getVisiblePolygons();
 
 for (var i = 0; i < polygons.length; i++) {
@@ -9,8 +10,9 @@ for (var i = 0; i < polygons.length; i++) {
 	console.log(`${kmArea} km^2`);
 }
 
-//
+// -----------------------------
 // Number of portals (and selection info)
+// -----------------------------
 var selection = window.plugin.cassandraNeutralizer.getSelectedPortals();
 
 // empty selection info
@@ -25,3 +27,41 @@ if (selection.polygons.length === 0) {
 		Portal(s): ${selection.portals.length}
 	`.replace(/\t+/g, ''));
 }
+
+
+// -----------------------------
+//  Collecting data for large areas (like for a whole city)
+//  Note! DO NOT try to do this for a country. Will probably crash your browser.
+// -----------------------------
+
+// 1. Disable clearing portals out of screen
+window.Render.prototype.clearPortalsOutsideBounds = function(bounds) {}
+// 2. Disable deleting portals from list at all
+window.Render.prototype.deletePortalEntity = function(guid) {}
+
+// -----------------------------
+//  Counting portals in large areas (partially or fully out of bounds)
+// -----------------------------
+
+// 1. Collect portals (use "Collecting data for large areas").
+
+// 2. Make sure you get all polygons, not just those fully visible
+window.plugin.cassandraNeutralizer.getVisiblePolygons = function() {
+	if (!window.plugin.drawTools) {
+		return [];
+	}
+
+	var visibleBounds = map.getBounds();
+
+	var polygons = [];
+	window.plugin.drawTools.drawnItems.eachLayer(function(layer) {
+		if (!(layer instanceof L.Polygon)) {
+			return;
+		}
+		polygons.push(layer);
+	});
+
+	return polygons;
+};
+
+// 3. Use script in section: "Number of portals (and selection info)".
